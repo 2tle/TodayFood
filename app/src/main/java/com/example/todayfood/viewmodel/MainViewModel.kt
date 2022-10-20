6339ce08b1f01aeb7427007ea10102f7ed0ae3a6
2tle <iam@2tle.io>
2022-10-20 20:11:51 +0900

    tired

---
+++ app/src/main/java/com/example/todayfood/viewmodel/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.todayfood.viewmodel;
+import android.view.View;
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel:ViewModel(){
+    lateinit var mySchoolName: LiveData<String>
+    lateinit var foodList : LiveData<String>
+
+    fun goToSchoolUpdateActivity(v: View) {
+
+    }
+}
